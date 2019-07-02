acc9f1b9ab5ddf3b677c223ed2eced135c379a0e
radikkaviev <radikkaviev83@yandex.ru>
2019-07-02 12:45:22 +0100

    Adding User model, adding AUTH_USER_MODEL to settings.py. Database migrations are next

--- app/core/models.py
+++ app/core/models.py
@@ -4,16 +4,30 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                        PermissionsMixin
 
+'''abstract class creates a model for the user'''
 class UserManager(BaseUserManager):
 
+    '''function creates and saves a new user model'''
     def create_user(self, email, password = None, **extra_fields):
-        '''function creates and saves a new user model'''
 
         user = self.model(email=email, **extra_fields)
 
         '''password must be encrypted, and can't be stored in above "user" variable '''
-        '''borrowing from AbstractBaseUser'''
         user.set_password(password)
         user.save(using=self._db)
 
         return user
+
+'''custom user model that supports using e-mail instead of username'''
+class User(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(max_length = 255, unique=True)
+    name = models.CharField(max_length = 255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    '''instantiate UserManager() for this particular User'''
+    objects = UserManager()
+
+    '''primary key: changing default USERNAME_FIELD from "username" to "email"'''
+    USERNAME_FIELD = 'email'
