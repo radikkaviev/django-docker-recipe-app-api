b99b06bbdce61da8ab18a2d357a0d8768ac1c681
radikkaviev <radikkaviev83@yandex.ru>
2019-07-13 18:51:06 +0100

    Adding tests for Admin Site, added code to make the test pass

--- app/core/models.py
+++ app/core/models.py
@@ -9,33 +9,25 @@ class UserManager(BaseUserManager):
 
     '''function creates and saves a new user model'''
     def create_user(self, email, password = None, **extra_fields):
-
         if not email:
             raise ValueError('Users must have an e-mail address')
-
         user = self.model(email=self.normalize_email(email), **extra_fields)
-
         '''password must be encrypted, and can't be stored in above "user" variable '''
         user.set_password(password)
         user.save(using = self._db)
-
         return user
 
     def create_superuser(self, email, password):
-        '''using this only in the command line, so don't need to worry about **extra_fields parameter'''
         '''Creates and saves a new super user'''
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
-        
         '''user is modified, so I need to save it again'''
         user.save(using = self._db)
-
         return user
 
 '''custom user model that supports using e-mail instead of username'''
 class User(AbstractBaseUser, PermissionsMixin):
-
     email = models.EmailField(max_length = 255, unique=True)
     name = models.CharField(max_length = 255)
     is_active = models.BooleanField(default=True)
---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,38 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        '''client: acts as a dummy web browser'''
+        self.client = Client()
+
+        self.admin_user = get_user_model().objects.create_superuser(
+            email = 'admin@softwareintrospectre.com',
+            password = 'password123'
+        )
+
+        '''admin is logged into the client'''
+        self.client.force_login(self.admin_user)
+
+        '''spare user for testing other things'''
+        self.user = get_user_model().objects.create_user(
+            email ='test@softwareintrospectre.com',
+            password = 'password123',
+            name = 'Test user full name'
+        )
+
+    '''necessary be acuse I need to slightly customize the Django admin to work with my Custom User Model (email instead of username)'''
+    def test_users_listed(self):
+        '''test that users are listed on user page'''
+
+        '''generates a URL for my list_user page, helpful if I want to change URL in the future'''
+        url = reverse('admin:core_user_changelist')
+
+        '''performs an HTTP GET-request, returns the response'''
+        resp = self.client.get(url)
+
+        '''checks for username, email. Also checks for HTTP response 200'''
+        self.assertContains(resp, self.user.name)
+        self.assertContains(resp, self.user.email)
